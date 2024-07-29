476ef1a811b3bc3ab034205d243d81972bc31b61
rajat106p <rajat106p@gmail.com>
2024-07-29 11:24:08 +0530

    remove unused reference

--- src/Pages/Stock/index.tsx
+++ src/Pages/Stock/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Data } from '../../Interfaces/Stock';
 import StockData from '../../Components/StockData';
 import DropDown from '../../Components/DropDown';
